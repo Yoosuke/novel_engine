5d29f392b8a16abe29bb686a3ae9a25f485d08d2
Yoosuke <yoosukenakao@thewaggle.co.jp>
2025-02-06 07:03:30 +0900

    mix bug package miss

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule NovelEngine.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "A LiveView-based visual novel engine",
+      package: package()
     ]
   end
 
@@ -21,8 +23,17 @@ defmodule NovelEngine.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:phoenix_live_view, "~> 0.20.0"},
+      {:phoenix_html, "~> 3.3"},
+      {:jason, "~> 1.4"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["YOSUKE NAKAO"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/Yoosuke/novel_engine.git"},
     ]
   end
 end
